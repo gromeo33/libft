76b27de43a5bc2c9fc64d5c0eb0f8404d4f977f3
gromeo33 <gromeo33@gmail.com>
2024-03-13 12:58:08 +0100

    modifica strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -20,10 +20,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - s2[i]);
 		++i;
 	}
 	if (i != n)
-		return (s1[i] - s2[i]);
+		return ((unsigned char)s1[i] - s2[i]);
 	return (0);
 }
