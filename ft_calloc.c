4cfa9f4315873546c3b135daa020744eef2a9c43
gromeo33 <gromeo33@gmail.com>
2024-03-18 18:21:23 +0100

    prova fine

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,18 +12,13 @@
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void    *ft_calloc(size_t c, size_t b)
 {
-	void			*mem;
-	size_t			i;
-	unsigned char	*temp;
+    void    *p;
 
-	mem = malloc(size * count);
-	if (!mem)
-		return (0);
-	temp = mem;
-	i = 0;
-	while (i++ < size * count)
-		*temp++ = 0;
-	return (mem);
+    p = malloc (c * b);
+    if (p == NULL)
+            return (p);
+    ft_bzero(p, b * c);
+    return (p);
 }
